60caf959f9696e8945f1d5f672b33e4da5226511
Kacper Bieganek <KacBieganek@gmail.com>
2018-03-26 00:37:10 +0200

    add StockDataFileWriter

--- rawstockdataconsumer/src/main/java/com/jackdaw/consumer/datastructures/ShiftingList.java
+++ rawstockdataconsumer/src/main/java/com/jackdaw/consumer/datastructures/ShiftingList.java
@@ -7,19 +7,19 @@ public class ShiftingList extends ArrayList {
 
     int maxSize;
 
-    public ShiftingList(int maxSize){
+    public ShiftingList(int maxSize) {
         super();
-        this.maxSize=maxSize;
+        this.maxSize = maxSize;
     }
 
     @Override
     public boolean add(Object o) {
-        if(this.size()==maxSize){
-            Collections.rotate(this,-1);
-            this.set(maxSize-1,o);
-        }else{
+        if (this.size() == maxSize) {
+            Collections.rotate(this, -1);
+            this.set(maxSize - 1, o);
+        } else {
             super.add(o);
         }
-      return true;
+        return true;
     }
 }
