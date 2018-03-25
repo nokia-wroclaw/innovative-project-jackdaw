6ad4c0b54cfa2cc42ee01611c7f7dd1b39023647
Kacper Bieganek <KacBieganek@gmail.com>
2018-03-25 21:32:05 +0200

    add shiftingList

---
+++ rawstockdataconsumer/src/main/java/com/jackdaw/consumer/datastructures/ShiftingList.java
@@ -0,0 +1,25 @@
+package com.jackdaw.consumer.datastructures;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ShiftingList extends ArrayList {
+
+    int maxSize;
+
+    public ShiftingList(int maxSize){
+        super();
+        this.maxSize=maxSize;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        if(this.size()==maxSize){
+            Collections.rotate(this,-1);
+            this.set(maxSize-1,o);
+        }else{
+            super.add(o);
+        }
+      return true;
+    }
+}
