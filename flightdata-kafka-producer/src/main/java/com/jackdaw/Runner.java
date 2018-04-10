bb041fb45259ed40876e5dcba0146ecbf1127886
Kacper Bieganek <KacBieganek@gmail.com>
2018-04-10 11:50:46 +0200

    add Dockerfile

--- flightdata-kafka-producer/src/main/java/com/jackdaw/Runner.java
+++ flightdata-kafka-producer/src/main/java/com/jackdaw/Runner.java
@@ -1,6 +1,8 @@
 package com.jackdaw;
 
+import com.jackdaw.avro.flights.Flight;
 import com.jackdaw.producer.FlightDataKafkaProducer;
+import org.apache.avro.Schema;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,10 +11,10 @@ import java.util.Properties;
 public class Runner {
 
     public static void main(String[] args) throws IOException {
+        Schema schema = Flight.getClassSchema();
         Properties props = new Properties();
         props.load(new FileInputStream("/workdir/topic.properties"));
         FlightDataKafkaProducer producer = new FlightDataKafkaProducer(props.get("fileName").toString(),props.get("topicName").toString());
-        Thread thread = new Thread(producer);
-        thread.start();
+        producer.runProducer();
     }
 }
