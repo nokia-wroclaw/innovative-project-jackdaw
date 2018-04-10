361c38cd5f36ac8626386742ce5ff649ddf5fd16
Kacper Bieganek <KacBieganek@gmail.com>
2018-04-10 12:08:09 +0200

    add pushing schema to SchemaRegistry

--- flightdata-kafka-producer/src/main/java/com/jackdaw/Runner.java
+++ flightdata-kafka-producer/src/main/java/com/jackdaw/Runner.java
@@ -2,6 +2,7 @@ package com.jackdaw;
 
 import com.jackdaw.avro.flights.Flight;
 import com.jackdaw.producer.FlightDataKafkaProducer;
+import okhttp3.*;
 import org.apache.avro.Schema;
 
 import java.io.FileInputStream;
@@ -9,12 +10,27 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Runner {
+    private final static MediaType SCHEMA_CONTENT =
+            MediaType.parse("application/vnd.schemaregistry.v1+json");
 
     public static void main(String[] args) throws IOException {
-        Schema schema = Flight.getClassSchema();
-        Properties props = new Properties();
-        props.load(new FileInputStream("/workdir/topic.properties"));
-        FlightDataKafkaProducer producer = new FlightDataKafkaProducer(props.get("fileName").toString(),props.get("topicName").toString());
+
+        Properties topicProperties = new Properties();
+        Properties schemaProps = new Properties();
+        final OkHttpClient client = new OkHttpClient();
+        final Schema schema = Flight.getClassSchema();
+
+        topicProperties.load(new FileInputStream("/workdir/topic.properties"));
+        schemaProps.load(new FileInputStream("/workdir/flightdata-kafka-producer.properties"));
+
+        Request request = new Request.Builder()
+                .post(RequestBody.create(SCHEMA_CONTENT, schema.toString()))
+                .url(schemaProps.getProperty("schema.registry.url").toString()+"/subjects/Flight/versions")
+                .build();
+
+        client.newCall(request).execute();
+        
+        FlightDataKafkaProducer producer = new FlightDataKafkaProducer(topicProperties.get("fileName").toString(),topicProperties.get("topicName").toString());
         producer.runProducer();
     }
 }
