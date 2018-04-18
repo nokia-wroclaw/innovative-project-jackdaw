5f90c8977e207e3eea526dc169915518fac60d63
ankanna <226168@student.pwr.edu.pl>
2018-04-18 18:53:33 +0200

    add geojson file

---
+++ visualisation/js/geojson/flights-geojson.js
@@ -0,0 +1,118 @@
+var estimatedDeparture =
+    {
+        "type": "FeatureCollection",
+        "features": [
+            { "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-51.1753811, -29.9934732]
+                },
+                "properties": {
+                    "color": "#aaaaaa",
+                    "flights": "AAL - 203",
+                    "companyAerial": "AMERICAN AIRLINES INC",
+                    "codeTypeLine": "Internacional",
+                    "departureExpected": "2016-01-30T08:58:00Z",
+                    "stateFlight": "Realizado",
+                    "codeJustification": "NA",
+                    "airportOrigin": "Alfonso Pena",
+                    "cityOrigin": "Sao Jose Dos Pinhais",
+                    "stateOrigin": "PR",
+                    "countryOrigin": "Brasil",
+                    "airportDestination": "Salgado Filho",
+                    "cityDestination": "Porto Alegre",
+                    "stateDestination": "RS",
+                    "countryDestination": "Brasil"
+                }
+            },
+        ]
+    }
+
+var realDeparture = {
+    "type": "FeatureCollection",
+    "features": [
+        { "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-51.1753811, -29.9934732]
+            },
+            "properties": {
+                "color": "#11bb11",
+                "flights": "AAL - 203",
+                "companyAerial": "AMERICAN AIRLINES INC",
+                "codeTypeLine": "Internacional",
+                "departureReal": "2016-01-30T08:58:00Z",
+                "stateFlight": "Realizado",
+                "codeJustification": "NA",
+                "airportOrigin": "Alfonso Pena",
+                "cityOrigin": "Sao Jose Dos Pinhais",
+                "stateOrigin": "PR",
+                "countryOrigin": "Brasil",
+                "airportDestination": "Salgado Filho",
+                "cityDestination": "Porto Alegre",
+                "stateDestination": "RS",
+                "countryDestination": "Brasil"
+            }
+        },
+    ]
+}
+
+var estimatedArrival = {
+    "type": "FeatureCollection",
+    "features": [
+        { "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-49.1724811, -25.5327132]
+            },
+            "properties": {
+                "color": "#cc350c",
+                "flights": "AAL - 203",
+                "companyAerial": "AMERICAN AIRLINES INC",
+                "codeTypeLine": "Internacional",
+                "arrivalExpected": "2016-01-30T10:35:00Z",
+                "stateFlight": "Realizado",
+                "codeJustification": "NA",
+                "airportOrigin": "Alfonso Pena",
+                "cityOrigin": "Sao Jose Dos Pinhais",
+                "stateOrigin": "PR",
+                "countryOrigin": "Brasil",
+                "airportDestination": "Salgado Filho",
+                "cityDestination": "Porto Alegre",
+                "stateDestination": "RS",
+                "countryDestination": "Brasil"
+            }
+        },
+    ]
+}
+
+var realArrival = {
+    "type": "FeatureCollection",
+    "features": [
+        { "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-51.1753811, -29.9934732], [-49.1724811, -25.5327132]
+                ]
+            },
+            "properties": {
+                "color": "#ef9115",
+                "flights": "AAL - 203",
+                "companyAerial": "AMERICAN AIRLINES INC",
+                "codeTypeLine": "Internacional",
+                "arrivalReal": "2016-01-30T10:35:00Z",
+                "stateFlight": "Realizado",
+                "codeJustification": "NA",
+                "airportOrigin": "Alfonso Pena",
+                "cityOrigin": "Sao Jose Dos Pinhais",
+                "stateOrigin": "PR",
+                "countryOrigin": "Brasil",
+                "airportDestination": "Salgado Filho",
+                "cityDestination": "Porto Alegre",
+                "stateDestination": "RS",
+                "countryDestination": "Brasil"
+            }
+        },
+    ]
+}
