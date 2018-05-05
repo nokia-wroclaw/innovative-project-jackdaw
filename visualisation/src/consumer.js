eecd5cba0d65276ad939c8f9feae95651d95fdfa
Hanna Grodzicka <hania.grodzicka@gmail.com>
2018-05-05 13:46:12 +0200

    Consumer fix

--- visualisation/src/consumer.js
+++ visualisation/src/consumer.js
@@ -4,17 +4,17 @@ const kafka = require('kafka-node');
 const Consumer = kafka.Consumer;
 const Offset = kafka.Offset;
 const Client = kafka.Client;
-const argv = require('optimist').argv;
-const topic = argv.topic || 'Visualisation';
+const topic = 'Visualization';
+const consumerGroupName = 'visualization-group';
 
-const client = new Client('localhost:2181');
-const topics = [{topic: topic, partition: 1}, {topic: topic, partition: 0}];
+const client = new Client('localhost:2181', consumerGroupName);
+const topics = [{topic: topic, partition: 0}];
 const options = {autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024};
 
 const consumer = new Consumer(client, topics, options);
 const offset = new Offset(client);
 
-consumer.on('message', function (message) {
+consumer.on('message', message => {
     console.log(message);
     switch (message.properties.timeType) {
         case "Expected arrival":
@@ -26,16 +26,16 @@ consumer.on('message', function (message) {
     }
 });
 
-consumer.on('error', function (err) {
-    console.log('error', err);
+consumer.on('error', err => {
+    console.error(`Error: ${err.stack || err.message}`, err);
 });
 
 /*
 * If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
 */
-consumer.on('offsetOutOfRange', function (topic) {
+consumer.on('offsetOutOfRange', topic => {
     topic.maxNum = 2;
-    offset.fetch([topic], function (err, offsets) {
+    offset.fetch([topic], (err, offsets) => {
         if (err) {
             return console.error(err);
         }
