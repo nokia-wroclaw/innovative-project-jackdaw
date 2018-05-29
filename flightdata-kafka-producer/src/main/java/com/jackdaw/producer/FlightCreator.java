7050cfc245a53073455829bcba1bf71c5d4c80c7
KacperBieganek <KacBieganek@gmail.com>
2018-05-29 20:17:21 +0200

    apply comment suggestion

--- flightdata-kafka-producer/src/main/java/com/jackdaw/producer/FlightCreator.java
+++ flightdata-kafka-producer/src/main/java/com/jackdaw/producer/FlightCreator.java
@@ -36,27 +36,28 @@ class FlightCreator {
         if (splitMessage.length != expectedArraySize) {
             return Optional.empty();
         } else {
-            builder.setFlightSymbol(splitMessage[FLIGHT_SYMBOL_INDEX]);
-            builder.setAirline(splitMessage[AIRLINE_INDEX]);
-            builder.setFlightType(FlightType.valueOf(splitMessage[FLIGHT_TYPE_INDEX]));
-            builder.setTimeType(TimeType.valueOf(splitMessage[TIME_TYPE_INDEX]));
-            builder.setTime(splitMessage[TIME_INDEX]);
-            builder.setFlightSituation(FlightSituation.valueOf(splitMessage[FLIGHT_SITUATION_INDEX]));
-            builder.setCodeJustification(splitMessage[CODE_JUSTIFICATION_INDEX]);
-            builder.setOriginAirport(splitMessage[ORIGIN_AIRPORT_INDEX]);
-            builder.setOriginCity(splitMessage[ORIGIN_CITY_INDEX]);
-            builder.setOriginState(splitMessage[ORIGIN_STATE_INDEX]);
-            builder.setOriginCountry(splitMessage[ORIGIN_COUNTRY_INDEX]);
-            builder.setDestinationAirport(splitMessage[DESTINATION_AIRPORT_INDEX]);
-            builder.setDestinationCity(splitMessage[DESTINATION_CITY_INDEX]);
-            builder.setDestinationState(splitMessage[DESTINATION_STATE_INDEX]);
-            builder.setDestinationCountry(splitMessage[DESTINATION_COUNTRY_INDEX]);
-            builder.setDestinationLongitude(NumberUtils.createDouble(splitMessage[DESTINATION_LONGITUDE_INDEX]));
-            builder.setDestinationLatitude(NumberUtils.createDouble(splitMessage[DESTINATION_LATITUDE_INDEX]));
-            builder.setOriginLongitude(NumberUtils.createDouble(splitMessage[ORIGIN_LONGITUDE_INDEX]));
-            builder.setOriginAltitude(NumberUtils.createDouble(splitMessage[ORIGIN_ALTITUDE_INDEX]));
+            return Optional.of(
+                    builder.setFlightSymbol(splitMessage[FLIGHT_SYMBOL_INDEX])
+                            .setAirline(splitMessage[AIRLINE_INDEX])
+                            .setFlightType(FlightType.valueOf(splitMessage[FLIGHT_TYPE_INDEX]))
+                            .setTimeType(TimeType.valueOf(splitMessage[TIME_TYPE_INDEX]))
+                            .setTime(splitMessage[TIME_INDEX])
+                            .setFlightSituation(FlightSituation.valueOf(splitMessage[FLIGHT_SITUATION_INDEX]))
+                            .setCodeJustification(splitMessage[CODE_JUSTIFICATION_INDEX])
+                            .setOriginAirport(splitMessage[ORIGIN_AIRPORT_INDEX])
+                            .setOriginCity(splitMessage[ORIGIN_CITY_INDEX])
+                            .setOriginState(splitMessage[ORIGIN_STATE_INDEX])
+                            .setOriginCountry(splitMessage[ORIGIN_COUNTRY_INDEX])
+                            .setDestinationAirport(splitMessage[DESTINATION_AIRPORT_INDEX])
+                            .setDestinationCity(splitMessage[DESTINATION_CITY_INDEX])
+                            .setDestinationState(splitMessage[DESTINATION_STATE_INDEX])
+                            .setDestinationCountry(splitMessage[DESTINATION_COUNTRY_INDEX])
+                            .setDestinationLongitude(NumberUtils.createDouble(splitMessage[DESTINATION_LONGITUDE_INDEX]))
+                            .setDestinationLatitude(NumberUtils.createDouble(splitMessage[DESTINATION_LATITUDE_INDEX]))
+                            .setOriginLongitude(NumberUtils.createDouble(splitMessage[ORIGIN_LONGITUDE_INDEX]))
+                            .setOriginAltitude(NumberUtils.createDouble(splitMessage[ORIGIN_ALTITUDE_INDEX]))
+                            .build());
         }
-        return Optional.of(builder.build());
     }
 }
 
