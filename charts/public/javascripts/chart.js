8a8f476d6759c6248baa6e98d2f46b6660bebb2f
hvvka <hania.grodzicka@gmail.com>
2018-06-20 20:07:16 +0200

    Add mocking delays (#37)
    * Add mocking delays
    
    * Cleanup

--- charts/public/javascripts/chart.js
+++ charts/public/javascripts/chart.js
@@ -1,12 +1,4 @@
-// Add new data on click – test
-// todo delete as a real data-source come into existence
-const dataPoints = [
-    {y: 10, label: "AZUL"},
-    {y: 5.4, label: "GOL"},
-    {y: 0.84, label: "UNITED AIRLINES"},
-    {y: 3.14, label: "KOREAN AIRLINES"},
-    {y: 4.76, label: "KLM ROYAL DUTCH AIRLINES"}
-];
+const dataPoints = [];
 
 const pieChart = new CanvasJS.Chart("pie", {
     animationEnabled: true,
@@ -49,17 +41,6 @@ window.onload = function () {
     pieChart.render();
     barChart.render();
 };
-
-// ------
-// Add new data on click – test
-// todo delete as a real data-source come into existence
-let message = "{ \"key\": \"1\", \"value\": [ \"1.34\", \"DELTA AIRLINES\" ] }";
-
-window.onclick = function () {
-    let data = JSON.parse(message);
-    updateChart(data);
-};
-// ------
 
 (function receiveMessage() {
     const socket = io.connect('http://0.0.0.0:3000', {transports: ['websocket', 'flashsocket']});
